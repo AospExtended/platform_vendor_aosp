835e3b1523383c71f2f003aec603e488ace1ce16
srisurya95 <srisurya95@gmail.com>
2016-03-28 10:55:47 +0530

    Add nfc
    Change-Id: I0024ff42c11294b346917092daf73d1b5b940cd7

---
+++ config/nfc_enhanced.mk
@@ -0,0 +1,4 @@
+# This device supports CM enhanced NFC
+PRODUCT_COPY_FILES += \
+    vendor/aospb/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
+
