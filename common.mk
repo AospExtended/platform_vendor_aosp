af6a9e61c3f84f61947019852abb36b74192b217
srisurya95 <srisurya95@gmail.com>
2016-07-28 02:21:04 -0400

    Add Substraum prebuilt

--- common.mk
+++ common.mk
@@ -63,6 +63,10 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
    vendor/aosp/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
    vendor/aosp/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
+# Substratum
+PRODUCT_COPY_FILES += \
+vendor/aosp/prebuilt/common/app/Substratum/Substratum.apk:system/app/Substratum/Substratum.apk
    
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
