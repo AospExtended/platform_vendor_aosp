4d3d01669ac787e42f2d83808913d1f3d5bacc5c
Steve Kondik <steve@cyngn.com>
2016-08-07 23:21:29 -0700

    cm: Add custom off-mode charging screen
      * Moving this to open-source from CyanogenOS :)
      * Thanks to Chao Chen, Ethan Chen, Pat Erley, Scott Mertz,
        and Keith Mok for various contributions to this.
    
    Change-Id: If79f9c279668d14ee0cde62889bb09e7185ef08c

--- common.mk
+++ common.mk
@@ -116,6 +116,15 @@ PRODUCT_PACKAGES += \
     Slimperience \
     AEXPapers
 
+# Custom off-mode charger
+ifneq ($(WITH_CM_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images \
+    font_log.png \
+    libhealthd.cm
+endif
+
 # DU Utils Library
 PRODUCT_PACKAGES += \
     org.dirtyunicorns.utils
