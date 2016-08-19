0b48189edb9ee15245bfd9c0ecf2f67b876b7747
srisurya95 <srisurya95@gmail.com>
2016-08-19 08:14:05 -0400

    Add back SuperSU 2.52 as an option for Sony devices.
    Ask @AdrianDC why it isn't flashing.

--- common.mk
+++ common.mk
@@ -60,9 +60,15 @@ PRODUCT_COPY_FILES +=  \
     vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 # SuperSU
+ifeq ($(BOARD_VENDOR),sony)
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/UPDATE-SuperSU-2.52.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/aosp/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+else
 PRODUCT_COPY_FILES += \
    vendor/aosp/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
    vendor/aosp/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+endif
 
 # Substratum
 PRODUCT_COPY_FILES += \
