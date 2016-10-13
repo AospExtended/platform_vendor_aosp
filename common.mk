1fc7e6c8595efc652367d222dc1f1df421668c65
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-13 08:18:27 +0200

    Vendor: Build telephony-ext for all devices
    TeleService (com.android.phone), which is built on all devices,
    also has dependencies on telephony-ext. As telephony-ext is
    currently absent on non-telephony devices (most tablets),
    FCs are rampant on these devices.
    
    Build telephony-ext for all device configurations to fix
    the dependencies.

--- common.mk
+++ common.mk
@@ -126,6 +126,13 @@ PRODUCT_PACKAGES += \
     CellBroadcastReceiver \
     Stk
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Mms depends on SoundRecorder for recorded audio messages
 PRODUCT_PACKAGES += \
     SoundRecorder
