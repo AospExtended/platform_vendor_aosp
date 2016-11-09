randomstuffpaul/platform_vendor_to

Update Pixel Launcher
Change-Id: I623bdcad5ac561f6f4e2f27ba26ec116d7c0c96a

config/common.mk
@@ -7,8 +7,8 @@ vendor/to/prebuilt/common/app/substratum.apk:system/app/substratum/substratum.ap
 
 #Pixel Launcher
 PRODUCT_COPY_FILES += \
-vendor/to/prebuilt/common/app/PixelLauncherPrebuilt.apk:system/priv-app/nexus-launcher/PixelLauncherPrebuilt.apk \
-vendor/to/prebuilt/common/app/WallpaperPickerGooglePrebuilt.apk:system/app/nexus-launcher/WallpaperPickerGooglePrebuilt.apk
+vendor/to/prebuilt/common/app/PixelLauncherPrebuilt.apk:system/priv-app/PixelLauncher/PixelLauncherPrebuilt.apk \
+vendor/to/prebuilt/common/app/WallpaperPickerGooglePrebuilt.apk:system/app/WallpaperPickerGoogle/WallpaperPickerGooglePrebuilt.apk
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \

AospExtended/platform_vendor_aosp

Change-Id: I188a61fc14da4a9c57f0b935cb6854949bef3ef9

common.mk
@@ -68,8 +68,8 @@ PRODUCT_COPY_FILES += \
 
 #Pixel Launcher
 PRODUCT_COPY_FILES += \
-	vendor/to/prebuilt/common/apk/PixelLauncherPrebuilt.apk:system/priv-app/pixel-launcher/PixelLauncherPrebuilt.apk \
-	vendor/to/prebuilt/common/apk/WallpaperPickerGooglePrebuilt.apk:system/app/pixel-launcher/WallpaperPickerGooglePrebuilt.apk
+	vendor/aosp/prebuilt/common/apk/PixelLauncherPrebuilt.apk:system/priv-app/PixelLauncher/PixelLauncherPrebuilt.apk \
+	vendor/aosp/prebuilt/common/apk/WallpaperPickerGooglePrebuilt.apk:system/app/WallpaperPickerGoogle/WallpaperPickerGooglePrebuilt.apk
 
 # MK Explorer
 PRODUCT_COPY_FILES += \