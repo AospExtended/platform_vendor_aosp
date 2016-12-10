9adfff59a4f72a09b8ab63a2c7d6eea50825c157
beanstown106 <nbenis106@gmail.com>
2016-12-10 11:45:32 -0500

    aosp_fixs: add new setup wizard rotation lock prop
    Conflicts:
    	configs/aosp_fixes.mk

--- common.mk
+++ common.mk
@@ -8,7 +8,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    ro.setupwizard.rotation_locked=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
