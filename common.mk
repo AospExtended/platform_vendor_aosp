c5a166443a7d3075feef1bbdb4791ddb3618d6db
Nate <nbenis106@gmail.com>
2016-12-18 04:09:42 +0100

    Enable assistant on all devices
    Change-Id: Id580170ecc415cf8c0076d3e1fe28f1ff9d11104

--- common.mk
+++ common.mk
@@ -14,10 +14,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
-# Enable Google Assistant on all devices.
-PRODUCT_PROPERTY_OVERRIDES += \
-   ro.opa.eligible_device=true
-
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
@@ -142,6 +138,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Substratum Verified
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.substratum.verified=true
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.opa.eligible_device=true
+
 
 # DU Utils Library
 PRODUCT_BOOT_JARS += \
