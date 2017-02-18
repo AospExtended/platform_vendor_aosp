namdeptrai2003/android_vendor_dnd

vendor: Switch to Magisk for root solution [2/2]
This will add the Magisk Manager into the ROM and move the zip to be
flashed during ROM installation. Solution is stable and actively
supported by its creator. Commit actually written by @nathanchance,
with authorship given to topjohnwu since he did everything that is
being added.

Start with Magisk v11.1 & Magisk Manager v4.1

Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

config/common.mk
@@ -248,6 +248,14 @@ PRODUCT_PACKAGES += \
     su
 endif
 
+# Magisk Manager
+PRODUCT_PACKAGES += \
+    MagiskManager
+
+# Copy Magisk zip
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/zip/magisk.zip:system/addon.d/magisk.zip
+
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
 PRODUCT_VERSION = 1.0

AospExtended/platform_vendor_aosp

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
Conflicts:
	configs/packages.mk

common.mk
@@ -69,6 +69,15 @@ PRODUCT_COPY_FILES += \
    vendor/aosp/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 endif
 
+# Magisk Manager
+PRODUCT_PACKAGES += \
+    MagiskManager
+
+# Copy Magisk zip
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/zip/magisk.zip:system/addon.d/magisk.zip
+
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml