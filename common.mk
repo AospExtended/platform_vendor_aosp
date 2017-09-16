5a2e8c04906e8d81352ad8d73d2f5d981799700d
Shubham Singh <coolsks94@gmail.com>
2017-09-16 10:41:53 +0530

    Finally v4.6 is here...
    probably the final nougat build
    
    Change-Id: I1231dbdbc6fc7e3aaf1b9bbf128c60f06e2af37d
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- common.mk
+++ common.mk
@@ -187,7 +187,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/aosp/config/aex_props.mk)
 
 #Extended Versioning
-EXTENDED_VERSION = v4.5
+EXTENDED_VERSION = v4.6
 
 ifndef EXTENDED_BUILD_TYPE
     EXTENDED_BUILD_TYPE := UNOFFICIAL
