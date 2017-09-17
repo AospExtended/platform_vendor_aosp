76618f2387f68dabb29331afa08867e3d04acea5
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-17 20:07:36 +0200

    (2/2) Fix soong flags
    This time for real
    
    Change-Id: I9604edfe1c5d45198bb84fdd8cad6f306cc15a4f
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,10 +1,10 @@
+# Insert new variables inside the Aosp structure
 aosp_soong:
-# Insert new variables inside the AOSP structure
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '"AOSP": {'; \
+	echo '"Aosp": {'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
-	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false),'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
