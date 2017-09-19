2cb412d0bceaba8141e46ba3964580185581a050
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-19 23:19:27 +0200

    Common: do not copy format.sh script
    It was used only on jf devices until N, actually
    not used on O.
    Remove it from vendor repo, we can add it later in jf
    device tree if needed
    
    Change-Id: If4a6372c96761f1568b46698b6a579eb23ea32de
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -32,10 +32,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     vendor/aosp/overlay/common \
     vendor/aosp/overlay/dictionaries
 
-# EXT4/F2FS format script
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/bin/format.sh:install/bin/format.sh
-
 # Custom AEX packages
 PRODUCT_PACKAGES += \
     BluetoothExt \
