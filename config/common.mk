3f22d4a70773083ae90338489f384510b57632f5
Christian Oder <myself5@carbonrom.org>
2017-09-29 22:48:51 +0200

    vendor: use SDClang 4.0
    Change-Id: I3cafca38b0684078fe88365afeb18fc5d3c9fe33

--- config/common.mk
+++ config/common.mk
@@ -170,7 +170,7 @@ USE_DEX2OAT_DEBUG ?= false
 
 # Include SDCLANG definitions if it is requested and available
 ifeq ($(HOST_OS),linux)
-    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
         include vendor/aosp/sdclang/sdclang.mk
     endif
 endif
