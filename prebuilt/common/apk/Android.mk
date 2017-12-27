26f80eea821c997c5952b0c0803c2a92b3c7b14d
Shubham Singh <coolsks94@gmail.com>
2017-12-27 08:30:36 +0530

    Add Retro Music Player
    Thanks to @h4h13 for this beautiful app
    
    https://play.google.com/store/apps/details?id=code.name.monkey.retromusic
    
    Change-Id: I99f91e61cc00d49510ceb2b662ec96a71fdf7783

--- prebuilt/common/apk/Android.mk
+++ prebuilt/common/apk/Android.mk
@@ -34,9 +34,9 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
 
-#Phonograph
+#Retro Music Player
 include $(CLEAR_VARS)
-LOCAL_MODULE := Phonograph
+LOCAL_MODULE := RetroMusicPlayer
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
