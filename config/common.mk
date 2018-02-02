23c6195783e82962c46adc5f298c627fa765eb0e
Wei Wang <wvw@google.com>
2018-02-02 13:01:38 -0800

    config: Add SysUI to PRODUCT_DEXPREOPT_SPEED_APPS
    Test: Build
    Change-Id: I088938462b389e1ebe783997f773dc5946345666

--- config/common.mk
+++ config/common.mk
@@ -94,6 +94,10 @@ PRODUCT_PACKAGES += \
     AEXPapers \
     ExactCalculator
 
+# Dex preopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUIGoogle
+
 # Pixel sysconfig
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/sysconfig/pixel.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel.xml
