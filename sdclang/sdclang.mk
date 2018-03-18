0e8dae8a9a1c07af2affb42f70736581a4c5e6cd
RJ Trujillo <certifiedblyndguy@gmail.com>
2018-03-18 13:13:34 -0700

    sdclang: Do not perform excessive debugging on user builds
    We really don't need all the information this produces, and it should
    decrease overall overhead in the end.
    
    Change-Id: If6028a1b339c9597e3ea717106c0db0bc66c3a89
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

--- sdclang/sdclang.mk
+++ sdclang/sdclang.mk
@@ -20,3 +20,7 @@ SDCLANG_PATH := vendor/qcom/sdclang-4.0/linux-x86/bin
 SDCLANG_LTO_DEFS := vendor/aosp/sdclang/sdllvm-lto-defs.mk
 
 SDCLANG_COMMON_FLAGS := -O3 -fvectorize -mllvm -polly-run-dce
+ifeq (user,$(TARGET_BUILD_VARIANT))
+  # Disable debugging on user builds
+  SDCLANG_COMMON_FLAGS := -g0 -DNDEBUG
+endif
