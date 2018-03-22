2b690df456b38a001dab17f195eeff56ef11af85
AmolAmrit <amol.amrit03@outlook.com>
2018-03-22 14:45:17 +0530

    extract_utols.sh: Fix smali path for AOSP
    Change-Id: I69873806263ece441501ba500ab35f3c34d233ca
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -690,8 +690,8 @@ function oat2dex() {
     local OAT=
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
-        export BAKSMALIJAR="$CM_ROOT"/vendor/cm/build/tools/smali/baksmali.jar
-        export SMALIJAR="$CM_ROOT"/vendor/cm/build/tools/smali/smali.jar
+        export BAKSMALIJAR="$CM_ROOT"/vendor/aosp/build/tools/smali/baksmali.jar
+        export SMALIJAR="$CM_ROOT"/vendor/aosp/build/tools/smali/smali.jar
     fi
 
     # Extract existing boot.oats to the temp folder
