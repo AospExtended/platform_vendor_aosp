4e4d043aa48ece9366ff5f23d95bccccdb3ac284
drpradsms <dr.pradsms@gmail.com>
2018-07-06 08:49:00 +0530

    Support Theming for some inbuilt apps [1/2]
    Themes- Dialer, Contacts, Filemanager
    
    Inspired by Domination theme
    
    Change-Id: Ibc4492b9ff8141fe4710da8036ec9cb5a9efe7c2

--- config/common.mk
+++ config/common.mk
@@ -102,6 +102,19 @@ PRODUCT_PACKAGES += \
     SystemChocolateTheme \
     SettingsChocolateTheme \
     DuiChocolateTheme \
+    ContactsThemeBlack \
+    DialerThemeBlack \
+    FilesThemeBlack \
+    ContactsThemeDark \
+    DialerThemeDark \
+    FilesThemeDark \
+    ContactsThemeChocolate \
+    DialerThemeChocolate \
+    FilesThemeChocolate \
+    ContactsThemeExtended \
+    DialerThemeExtended \
+    FilesThemeExtended \
+
 
 # Fonts
 PRODUCT_PACKAGES += \
