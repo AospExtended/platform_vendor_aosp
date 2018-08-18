84f119b1c75c4fd05e6154da7d6963872ed9627e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-18 12:24:59 +0000

    Add WellbeingPrebuilt
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -59,7 +59,8 @@ PRODUCT_PACKAGES += \
     AEXPapers \
     RetroMusicPlayer \
     WallpaperPickerGoogle \
-    MarkupGoogle
+    MarkupGoogle \
+    WellbeingPrebuilt
 
 # Markup libs
 PRODUCT_COPY_FILES += \
