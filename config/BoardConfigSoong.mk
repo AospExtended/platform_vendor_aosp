00d6d42a4c53b3077568fe612cc3e9acb7bd3f7e
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-18 21:53:24 -0700

    Aosp: Dynamically generate kernel headers using aosp generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

---
+++ config/BoardConfigSoong.mk
@@ -0,0 +1,22 @@
+# Add variables that we wish to make available to soong here.
+EXPORT_TO_SOONG := \
+    KERNEL_ARCH \
+    KERNEL_CROSS_COMPILE \
+    KERNEL_MAKE_FLAGS \
+    TARGET_KERNEL_CONFIG \
+    TARGET_KERNEL_SOURCE
+
+# Setup SOONG_CONFIG_* vars to export the vars listed above.
+# Documentation here:
+# https://github.com/AospExtended/platform_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
+
+SOONG_CONFIG_NAMESPACES += aospVarsPlugin
+
+SOONG_CONFIG_aospVarsPlugin :=
+
+define addVar
+  SOONG_CONFIG_aospVarsPlugin += $(1)
+  SOONG_CONFIG_aospVarsPlugin_$(1) := $$($1)
+endef
+
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
