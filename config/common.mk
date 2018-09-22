e1fd87ca7dad6289319e8ab0ca2c7411ec6b0428
Alex Cruz <du.alexcruz@gmail.com>
2018-09-22 12:12:39 +0000

    Fruity pebbles [2/4]
    Change-Id: I3e6e73b61c4a292c743f93a47dc39510afb88f8a

--- config/common.mk
+++ config/common.mk
@@ -62,6 +62,63 @@ PRODUCT_PACKAGES += \
     MarkupGoogle \
     WellbeingPrebuilt
 
+# Themes
+# Accents
+PRODUCT_PACKAGES += \
+    PixelTheme \
+    AmberAccent \
+    BlueAccent \
+    BlueGreyAccent \
+    BrownAccent \
+    CyanAccent \
+    CandyRedAccent \
+    DeepOrangeAccent \
+    DeepPurpleAccent \
+    ExtendedGreenAccent \
+    GreenAccent \
+    GreyAccent \
+    IndigoAccent \
+    JadeGreenAccent \
+    LightBlueAccent \
+    LightGreenAccent \
+    LimeAccent \
+    OrangeAccent \
+    PaleBlueAccent \
+    PaleRedAccent \
+    PinkAccent \
+    PurpleAccent \
+    RedAccent \
+    TealAccent \
+    YellowAccent
+
+# UI Variants
+PRODUCT_PACKAGES += \
+    SystemDarkTheme \
+    SettingsDarkTheme \
+    SystemUIDarkTheme \
+    SystemBlackTheme \
+    SettingsBlackTheme \
+    SystemUIBlackTheme \
+    SystemExtendedTheme \
+    SettingsExtendedTheme \
+    SystemUIExtendedTheme \
+    SystemChocolateTheme \
+    SettingsChocolateTheme \
+    SystemUIChocolateTheme \
+    ContactsThemeBlack \
+    DialerThemeBlack \
+    FilesThemeBlack \
+    ContactsThemeDark \
+    DialerThemeDark \
+    FilesThemeDark \
+    ContactsThemeChocolate \
+    DialerThemeChocolate \
+    FilesThemeChocolate \
+    ContactsThemeExtended \
+    DialerThemeExtended \
+    FilesThemeExtended \
+
+
 # Markup libs
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
