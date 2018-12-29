748b32b6d24716ec2d067b06af78a8b927dacaf7
Michael Bestas <mkbestas@lineageos.org>
2018-12-29 23:34:17 +0200

    soong_config: Add flag for msm8974 1440p EGL workaround
    Change-Id: Iad2df3dad223604f1e2c454432b9c9239db8d035

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -17,6 +17,9 @@ type Product_variables struct {
 	Device_support_wait_for_qsee struct {
 		Cflags []string
 	}
+	Apply_msm8974_1440p_egl_workaround struct {
+		Cflags []string
+	}
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
@@ -55,6 +58,7 @@ type ProductVariables struct {
 	Device_support_hwfde_perf  *bool `json:",omitempty"`
 	Device_support_legacy_hwfde  *bool `json:",omitempty"`
 	Device_support_wait_for_qsee  *bool `json:",omitempty"`
+	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
