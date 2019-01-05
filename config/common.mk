b935e14e75b1b1fee5d4297dba54dc9d891fdfc6
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-05 11:46:23 +0100

    Disable art debug and enable minimize debug info
    Change-Id: I12b65c532201bf31f12cfb1d62c4a8bbe2204e2f

--- config/common.mk
+++ config/common.mk
@@ -55,6 +55,14 @@ PRODUCT_PACKAGES += \
     Jelly
 endif
 
+# Do not include art debug targets
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
 PRODUCT_PACKAGES += \
     Terminal \
     LatinIME \
