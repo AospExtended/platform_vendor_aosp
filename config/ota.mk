PixelExperience/vendor_aosp

vendor: Disable uncrypt for ota
Change-Id: Ie2cae78b9d8fea247af4fa741421b9b90f5c8ece

config/ota.mk
@@ -3,7 +3,8 @@ ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 CUSTOM_OTA_VERSION_CODE := pie_caf
 
 CUSTOM_PROPERTIES += \
-    org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
+    org.pixelexperience.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
+    sys.ota.disable_uncrypt=1
 
 PRODUCT_PACKAGES += \
     Updates

AospExtended/platform_vendor_aosp

Change-Id: I5d9615f71e68d20ab4407726c9d7a1ad53287241

config/ota.mk
@@ -21,7 +21,8 @@ CUSTOM_OTA_VERSION_CODE := pie
 endif
 
 PRODUCT_GENERIC_PROPERTIES += \
-  ro.extended.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
+  ro.extended.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
+  sys.ota.disable_uncrypt=1
 
 PRODUCT_PACKAGES += \
         Updates