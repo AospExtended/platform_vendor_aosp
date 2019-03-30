247568d5a1cf5cdf9f37fe103d0374ee20a8e917
AmulyaX <amulya.b520@gmail.com>
2019-03-30 18:49:51 +0000

    New AospExtended BootAnimation by @AndroBuddy007
    People who used to set TARGET_BOOT_ANIMATION_RES flag to 2140 for FHD+ resolution, kindly change it to 2160.
    
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>
    Change-Id: Ife7df0f48b9e6484a15de7ad6118468395b3be36

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -26,8 +26,8 @@ else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
      PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1440.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),2140)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080-2140-18-9.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),2160)
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080-2160-18-9.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using generic bootanimation )
      PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
