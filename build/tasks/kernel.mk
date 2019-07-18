32e0795c850b0c0bf8ba33ca298f1f75dfadf3ed
AmolAmrit <amol.amrit03@outlook.com>
2019-07-18 20:18:40 +0200

    kernel: Sync with LOS properly
    * Recent upstream commits weer giving build errors
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    Change-Id: I40165dab9b12babd7c24173bf1b90d5ec32df31d

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -169,8 +169,6 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
         KERNEL_CLANG_VERSION := $(LLVM_PREBUILTS_VERSION)
     endif
     TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(KERNEL_CLANG_VERSION)/bin
-    KBUILD_COMPILER_STRING := $(shell $(TARGET_KERNEL_CLANG_PATH)/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
-    export KBUILD_COMPILER_STRING
     ifeq ($(KERNEL_ARCH),arm64)
         KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
     else ifeq ($(KERNEL_ARCH),arm)
@@ -244,7 +242,7 @@ TARGET_KERNEL_BINARIES: $(KERNEL_CONFIG)
 		fi
 
 .PHONY: INSTALLED_KERNEL_MODULES
-INSTALLED_KERNEL_MODULES: depmod
+INSTALLED_KERNEL_MODULES: depmod-host
 	$(hide) if grep -q '=m' $(KERNEL_CONFIG); then \
 			echo "Installing Kernel Modules"; \
 			$(call make-kernel-target,INSTALL_MOD_PATH=$(MODULES_INTERMEDIATES) modules_install); \
