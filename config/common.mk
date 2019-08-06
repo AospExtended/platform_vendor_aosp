e787d8b8c6c8b66c3128f143fda03a07913c4b2a
Gaurav Singh <allstargaurav@gmail.com>
2019-08-06 15:51:47 +0200

    Remove Prebuilt Wellbeing
    Change-Id: I38d862dee39dc872f88acad828e255ea530fd840

--- config/common.mk
+++ config/common.mk
@@ -87,7 +87,6 @@ PRODUCT_PACKAGES += \
     RetroMusicPlayer \
     WallpaperPickerGoogle \
     MarkupGoogle \
-    WellbeingPrebuilt \
     Recorder \
     WeatherClient \
     SubstratumSignature
@@ -185,7 +184,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     HideCutout \
     StatusBarStock
-    
+
 # Markup libs
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
