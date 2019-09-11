2e45b0328813013a55f4d8cd1901171730f0ec57
SagarMakhar <sagarmakhar@gmail.com>
2019-09-11 15:39:04 +0000

    Move bootanimations to system/product
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Id03a10fb3b86473ceaa4e75c228f0fa982cc0687

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -17,23 +17,23 @@
 TARGET_BOOT_ANIMATION_RES ?= undefined
 
 ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-480.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-480.zip:system/product/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),540)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-540.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-540.zip:system/product/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-720.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-720.zip:system/product/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080.zip:system/product/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1440.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1440.zip:system/product/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),2140)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080-2140-18-9.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080-2140-18-9.zip:system/product/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),2280)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080-2280-19-9.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-1080-2280-19-9.zip:system/product/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using generic bootanimation )
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/bootanimation.zip:system/product/media/bootanimation.zip
 else
      $(warning Defined bootanimation res is wrong, using generic bootanimation )
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/bootanimation.zip:system/product/media/bootanimation.zip
 endif
