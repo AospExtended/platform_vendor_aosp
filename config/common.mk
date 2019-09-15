3effe82fc4c8a61bcfcacb66f9618c070ca0a16c
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-15 16:03:53 +0530

    vendor: fonts: Update fonts to Android 10 and change output dir
    Extracted from https://github.com/AndroidDumps/google_taimen_dump/tree/taimen-user-10-QP1A.190711.020-5800535-release-keys/system/system/product/fonts
    
    Change-Id: I7fd353cb2991caa1cb9b7b5dfed86225db44ef86
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -191,6 +191,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES +=  \
     vendor/aosp/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
+# Fonts
+include vendor/aosp/config/fonts.mk
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
