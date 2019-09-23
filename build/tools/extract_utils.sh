e5e7f0fb8f58742b998f8736cf50c937a13bf5aa
Bruno Martins <bgcngm@gmail.com>
2019-09-23 11:51:33 +0100

    extract_utils: Source the prebuilts from the new repo
    The prebuilt tools now live in prebuilts/tools-aosp.
    
    Change-Id: I4022210c52e1002aae0c6e622a480e5c7e10c366

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -1192,17 +1193,16 @@ function oat2dex() {
     local HOST="$(uname)"
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
-
-        export BAKSMALIJAR="$AOSP_ROOT"/vendor/aosp/build/tools/smali/baksmali.jar
-        export SMALIJAR="$AOSP_ROOT"/vendor/aosp/build/tools/smali/smali.jar
+        export BAKSMALIJAR="$AOSP_ROOT"/prebuilts/tools-aosp/common/smali/baksmali.jar
+        export SMALIJAR="$AOSP_ROOT"/prebuilts/tools-aosp/common/smali/smali.jar
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$AOSP_ROOT"/vendor/aosp/build/tools/"$HOST"/vdexExtractor
+        export VDEXEXTRACTOR="$AOSP_ROOT"/prebuilts/tools-aosp/"${HOST,,}"-x86/bin/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$AOSP_ROOT"/vendor/aosp/build/tools/"$HOST"/compact_dex_converter
+        export CDEXCONVERTER="$AOSP_ROOT"/prebuilts/tools-aosp/"${HOST,,}"-x86/bin/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
