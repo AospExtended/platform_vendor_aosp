d1f8e81fd76ba906d1feb2ab73c55eddaff72614
dianlujitao <dianlujitao@lineageos.org>
2019-10-08 14:46:58 +0800

    soong_config: Reverse wait for qsee flag
     * Disabled by QCOM on P and onwards
    
    Change-Id: I3f27d399293a95ad75446482c8711adf5fed1f78

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -14,7 +14,7 @@ type Product_variables struct {
 	Device_support_hwfde_perf struct {
 		Cflags []string
 	}
-	Should_skip_waiting_for_qsee struct {
+	Should_wait_for_qsee struct {
 		Cflags []string
 	}
 	Apply_msm8974_1440p_egl_workaround struct {
@@ -67,7 +67,7 @@ type ProductVariables struct {
 	Bootloader_message_offset  *int `json:",omitempty"`
 	Device_support_hwfde  *bool `json:",omitempty"`
 	Device_support_hwfde_perf  *bool `json:",omitempty"`
-	Should_skip_waiting_for_qsee  *bool `json:",omitempty"`
+	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
