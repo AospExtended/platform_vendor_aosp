32a0976060d766e18816096d519f3a7b1962aa44
Arghya Chanda <arghyac35@gmail.com>
2020-01-15 14:18:40 +0530

    Move all font related stuffs to our overlays repo[2/3]
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: Id1fafb7b788406d19a3f84182b4115e757f2da84

--- config/fonts.mk
+++
@@ -1,6 +0,0 @@
-# Fonts
-LOCAL_PATH := vendor/aosp/fonts
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
