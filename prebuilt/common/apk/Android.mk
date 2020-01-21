54fc7f044a4a40e081b4c250d7f97643096a24f8
Gaurav Singh <allstargaurav@gmail.com>
2020-01-21 21:26:04 +0530

    Bruh
    Change-Id: Ifa2dcef7f6b760e573224eb09877c7f7e5cb1b1a

--- prebuilt/common/apk/Android.mk
+++ prebuilt/common/apk/Android.mk
@@ -24,16 +24,6 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
 
-#Retro Music Player
-include $(CLEAR_VARS)
-LOCAL_MODULE := RetroMusicPlayer
-LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_DEX_PREOPT := false
-include $(BUILD_PREBUILT)
-
 ifneq ($(WITH_GAPPS),true)
 
 #ViaBrowser
