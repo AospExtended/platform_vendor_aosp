843caaca47e98dbf0d3e3e67cad1e4594faac983
Alex Cruz <mazdarider23@gmail.com>
2020-10-09 04:38:55 +0000

    Eye candy for device/lunch menu
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -12,6 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Build system colors
+
+ifneq ($(BUILD_WITH_COLORS),0)
+  CL_RED="\033[31m"
+  CL_YLW="\033[33m"
+  CL_RST="\033[0m"
+endif
+
 # It's called md5 on Mac OS and md5sum on Linux
 ifeq ($(HOST_OS),darwin)
 MD5:=md5 -q
@@ -27,8 +35,17 @@ bacon: aex
 aex: otapkg
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(TARGET_PACKAGE)
 	$(hide) $(MD5) $(TARGET_PACKAGE) > $(TARGET_PACKAGE).md5sum
-	@echo -e ${CL_BLD}${CL_CYN}"===============================-Package complete-==============================="${CL_RST}
+	@echo -e ${CL_RST} ""
+	@echo -e ${CL_RST} " ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄  ▄▄▄▄▄▄▄ ▄▄▄▄▄▄  "
+	@echo -e ${CL_RST} "█       █       █       █       █  █       █  █▄█  █       █       █  █  █ █      ██       █      █ "
+	@echo -e ${CL_RST} "█   ▄   █   ▄   █  ▄▄▄▄▄█    ▄  █  █    ▄▄▄█       █▄     ▄█    ▄▄▄█   █▄█ █  ▄    █    ▄▄▄█  ▄    █"
+	@echo -e ${CL_RST} "█  █▄█  █  █ █  █ █▄▄▄▄▄█   █▄█ █  █   █▄▄▄█       █ █   █ █   █▄▄▄█       █ █ █   █   █▄▄▄█ █ █   █"
+	@echo -e ${CL_RST} "█       █  █▄█  █▄▄▄▄▄  █    ▄▄▄█  █    ▄▄▄██     █  █   █ █    ▄▄▄█  ▄    █ █▄█   █    ▄▄▄█ █▄█   █"
+	@echo -e ${CL_RST} "█   ▄   █       █▄▄▄▄▄█ █   █      █   █▄▄▄█   ▄   █ █   █ █   █▄▄▄█ █ █   █       █   █▄▄▄█       █"
+	@echo -e ${CL_RST} "█▄▄█ █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄█      █▄▄▄▄▄▄▄█▄▄█ █▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄█  █▄▄█▄▄▄▄▄▄██▄▄▄▄▄▄▄█▄▄▄▄▄▄█ "
+	@echo -e ${CL_RST} "" ${CL_RST}
+	@echo -e ${CL_BLD}${CL_RED}"=======================================-Package complete-======================================="${CL_RED}
 	@echo -e ${CL_BLD}${CL_YLW}"Zip: "${CL_YLW} $(TARGET_PACKAGE)${CL_RST}
 	@echo -e ${CL_BLD}${CL_YLW}"MD5: "${CL_YLW}" `cat $(TARGET_PACKAGE).md5sum | awk '{print $$1}' `"${CL_RST}
 	@echo -e ${CL_BLD}${CL_YLW}"Size:"${CL_YLW}" `du -sh $(TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
-	@echo -e ${CL_BLD}${CL_CYN}"================================================================================"${CL_RST}
+	@echo -e ${CL_BLD}${CL_RED}"================================================================================================"${CL_RST}
