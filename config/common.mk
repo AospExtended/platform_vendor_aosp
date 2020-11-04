1d7f69adf28e69ec8c25e97850f97791af26ce32
Danny Lin <danny@kdrag0n.dev>
2020-11-03 22:47:28 -0800

    config: Build simple DeviceConfig provisioning service
    We need a simple service to provision DeviceConfig for setting various
    config defaults, like enabling permission hub, without modifying the
    AOSP source code directly. Build our custom service to do the
    provisioning from resource overlay values instead.
    
    Change-Id: Id2910d799b503c339f56e7f92a5e5f3ba93a5296

--- config/common.mk
+++ config/common.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     QuickAccessWallet
 
+# Config
+PRODUCT_PACKAGES += \
+    SimpleDeviceConfig
+
 # Cutout control overlays
 PRODUCT_PACKAGES += \
     HideCutout \
