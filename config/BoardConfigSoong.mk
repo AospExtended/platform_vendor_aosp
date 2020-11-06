aa6251778f9e436fb8f2c70d89315f9bfbaf8b2d
Arne Coucheron <arco68@gmail.com>
2020-11-07 00:46:10 +0100

    soong: Add flag for disabling postrender cleanup
    Some legacy devices experiences bad lag unless this is disabled.
    
    Disable it with:
    TARGET_DISABLE_POSTRENDER_CLEANUP := true
    
    Change-Id: I2b40dc4cbcab06fb293d1f407321af2fae846a51

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += aospGlobalVars
 SOONG_CONFIG_aospGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
@@ -61,6 +62,7 @@ SOONG_CONFIG_aospQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_aospGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_aospGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_aospGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_aospGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
