7105cf4ec21bf6e4079b1ce64ad000d8ecf35152
sb6596 <shubhamprince111@gmail.com>
2021-05-13 08:54:12 +0000

    config: Build ThemePicker
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Change-Id: Iafb08a1f091761ce535ca2ad249cc3d30fd051da

--- config/common.mk
+++ config/common.mk
@@ -192,6 +192,9 @@ PRODUCT_PACKAGES += \
 # ThemeOverlays
 include packages/overlays/Themes/themes.mk
 
+PRODUCT_PACKAGES += \
+    ThemePicker
+
 # Recommend using the non debug dexpreopter
 USE_DEX2OAT_DEBUG := false
 
