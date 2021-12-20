26bc246dceb18d0790c4fa04c5943c84d1a7cdd1
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-12-20 11:54:28 +0000

    vendor: soong: Add camera_needs_client_info_lib
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Change-Id: Ie69bac2adb8150f6954730907a9ec447877647cb

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_aospGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_needs_client_info \
+    camera_needs_client_info_lib \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -70,6 +71,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_aospGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_aospGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_aospGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_aospGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_aospGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
