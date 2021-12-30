0bc5c36ff27429cf79967200b607572197136eae
Alexander Koskovich <zvnexus@outlook.com>
2021-12-30 20:14:54 +0530

    vendor: aosp: config: Add framework compatibility matrix for Lineage HALs.
    * Cover all possible Lineage HALs here instead of covering
       them in the device specific manifests.
    
    * AEX: Strip it down for our needs.
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    Change-Id: I938f14920bc5ea24278218a5081d63b2f7d96d4f

--- config/common.mk
+++ config/common.mk
@@ -20,6 +20,8 @@ include vendor/aosp/config/version.mk
 # Audio files
 $(call inherit-product, vendor/aosp/config/google_audio.mk)
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
+
 # Conditionally build adb root
 #ifneq ($(TARGET_BUILD_VARIANT),user)
 #PRODUCT_PACKAGES += \
