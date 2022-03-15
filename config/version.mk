50186fd20cdbd6af2e808be7018facf91b9b3624
sb6596 <shubhamprince111@gmail.com>
2022-03-15 04:17:15 +0000

    Bump to v9.1
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 #Extended Versioning
-EXTENDED_VERSION = v9.0
+EXTENDED_VERSION = v9.1
 EXTENDED_BUILD_TYPE ?= UNOFFICIAL
 
 ifeq ($(EXTENDED_BUILD_TYPE), OFFICIAL)
